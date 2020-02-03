ff6c9e3ab11cea4616e7e052f206fac6cd989af0
Kaden <02kaden.seeger@gulllakecs.org>
2020-02-03 09:59:49 -0500

    Added some of the java might use this layout

--- app/src/main/java/com/example/destiny2score/MainActivity.java
+++ app/src/main/java/com/example/destiny2score/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.destiny2score;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+
+
+    private void displayredpone(int number){
+        TextView scoreredpone = findViewById(R.id.scoreredpone);
+        scoreredpone.setText("" + number);
+    }private void displayredtwo(int number){
+        TextView scoreredptwo = findViewById(R.id.scoreredptwo);
+        scoreredptwo.setText("" + number);
+    }private void displayredthree(int number){
+        TextView scoreredpthree = findViewById(R.id.scoreredpthree);
+        scoreredpthree.setText("" + number);
     }
 }
